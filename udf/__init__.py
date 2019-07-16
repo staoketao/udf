7d21df853f58d42aa83f23216d7bbc91bd9c19ba
Tao, Sha <Sha.Tao@moodys.com>
2019-07-16 10:24:26 +0000

    remove folder

--- udf/__init__.py
+++
@@ -1,1 +0,0 @@
-from udf import Exp
--- udf/udf.py
+++
@@ -1,5 +0,0 @@
-ass Exp:
-    def __init__(self):
-    
-    def showStats(self):
-        print(self.shape, self.head(2),self.dtypes,self.describe(include='all'))
